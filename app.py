e0ec26b19ab8c877c6d3686d50be8be1c6697e46
washim <mdwashimmomin04x@gmail.com>
2025-10-06 15:49:48 +0530

    linting issues corrected

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-def add(a,b):
+def add(a, b):
     return a + b
 
 
-
-
-def subtract(a,b): return a - b
+def subtract(a, b):
+    return a - b
 
 
 result1 = add(5, 3)
 result2 = subtract(10, 4)
-
-
